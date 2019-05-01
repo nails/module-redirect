094fc2d66096cda2b8c8945f4b3c0b1b0d91ecfd
Pablo de la Peña <p@shedcollective.org>
2019-05-01 11:22:27 +0100

    Centralising event namespaces

---
+++ src/Event/Listener/System/Ready.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * The Ready event listener
+ *
+ * @package  App
+ * @category events
+ */
+
+namespace Nails\Redirect\Listener\System;
+
+use Nails\Common;
+use Nails\Common\Events\Subscription;
+use Nails\Factory;
+
+
+/**
+ * Class Ready
+ *
+ * @package App\Event\Listener\User
+ */
+class Ready extends Subscription
+{
+    /**
+     * Ready constructor.
+     */
+    public function __construct()
+    {
+        $this
+            ->setEvent(Common\Events::SYSTEM_READY)
+            ->setCallback([$this, 'execute']);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Called when the event is triggered
+     *
+     * @throws FactoryException
+     * @throws ModelException
+     */
+    public function execute(): void
+    {
+        $oInput = Factory::service('Input');
+        $oModel = Factory::model('Redirect', 'nails/module-redirect');
+
+        $sUri = $oInput->server('REQUEST_URI');
+        $sUri = '/' . trim($sUri, '/');
+
+        /**
+         * Using PDO to craft our own query as CI manipulates the string if there's
+         * a query string when attempting to protect identifiers.
+         */
+        $oPdoDb        = Factory::service('PDODatabase');
+        $oPdoStatement = $oPdoDb->prepare('SELECT * FROM `' . $oModel->getTableName() . '` WHERE old_url = :old_url');
+        $oPdoStatement->execute(['old_url' => $sUri]);
+        $aResults = $oPdoStatement->fetchAll(\PDO::FETCH_OBJ);
+
+        if (!empty($aResults)) {
+            $oRedirect = reset($aResults);
+            //  Avoid loops
+            if ($sUri !== $oRedirect->new_url) {
+                redirect($oRedirect->new_url, 'location', $oRedirect->type);
+            }
+        }
+    }
+}
--- src/Events.php
+++ src/Events.php
@@ -2,50 +2,15 @@
 
 namespace Nails\Redirect;
 
-use Nails\Common;
 use Nails\Common\Events\Base;
-use Nails\Factory;
+use Nails\Redirect\Event\Listener\System;
 
 class Events extends Base
 {
     public function autoload()
     {
         return [
-            Factory::factory('EventSubscription')
-                   ->setEvent(Common\Events::SYSTEM_READY)
-                   ->setNamespace('nails/common')
-                   ->setCallback([$this, 'redirects']),
+            new System\Ready(),
         ];
-    }
-
-    // --------------------------------------------------------------------------
-
-    /**
-     * Facilitate redirects
-     */
-    public function redirects()
-    {
-        $oInput = Factory::service('Input');
-        $oModel = Factory::model('Redirect', 'nails/module-redirect');
-
-        $sUri = $oInput->server('REQUEST_URI');
-        $sUri = '/' . trim($sUri, '/');
-
-        /**
-         * Using PDO to craft our own query as CI manipulates the string if there's
-         * a query string when attempting to protect identifiers.
-         */
-        $oPdoDb        = Factory::service('PDODatabase');
-        $oPdoStatement = $oPdoDb->prepare('SELECT * FROM `' . $oModel->getTableName() . '` WHERE old_url = :old_url');
-        $oPdoStatement->execute(['old_url' => $sUri]);
-        $aResults = $oPdoStatement->fetchAll(\PDO::FETCH_OBJ);
-
-        if (!empty($aResults)) {
-            $oRedirect = reset($aResults);
-            //  Avoid loops
-            if ($sUri !== $oRedirect->new_url) {
-                redirect($oRedirect->new_url, 'location', $oRedirect->type);
-            }
-        }
     }
 }
