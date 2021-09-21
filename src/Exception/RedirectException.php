71c7b49d7406cdd438e5556cdf66756eb3000eb0
Pablo de la Peña <p@shedcollective.org>
2021-09-21 17:00:24 +0100

    Add RedirectException

---
+++ src/Exception/RedirectException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nails\Redirect\Exception;
+
+use Nails\Common\Exception\NailsException;
+
+/**
+ * Class RedirectException
+ *
+ * @package Nails\Redirect\Exception
+ */
+class RedirectException extends NailsException
+{
+}
