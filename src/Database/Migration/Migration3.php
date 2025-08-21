80e9f55fd120a41f4df858d7b9badec71f45e46a
Pablo de la Peña <p@shedcollective.org>
2025-08-21 10:59:03 +0100

    feat: Change table to by uf8mb4

---
+++ src/Database/Migration/Migration3.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Migration: 3
+ * Started:   21/08/2025
+ */
+
+namespace Nails\Redirect\Database\Migration;
+
+use Nails\Common\Interfaces;
+use Nails\Common\Traits;
+use Nails\Redirect\Admin\Permission;
+
+/**
+ * Class Migration3
+ *
+ * @package Nails\Cms\Database\Migration
+ */
+class Migration3 implements Interfaces\Database\Migration
+{
+    use Traits\Database\Migration;
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Execute the migration
+     */
+    public function execute(): void
+    {
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHARACTER SET = utf8mb4;');
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `old_url` `old_url` VARCHAR(500) CHARACTER SET utf8mb4 NULL DEFAULT NULL;');
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `new_url` `new_url` VARCHAR(500) CHARACTER SET utf8mb4 NULL DEFAULT NULL;');
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `type` `type` ENUM('301','302') CHARACTER SET utf8mb4 NULL DEFAULT '301';');
+    }
+}
