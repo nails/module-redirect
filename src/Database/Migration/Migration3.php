529fdc8bdcdf9b4b67a2011613fd66f5faefa660
Pablo de la Peña <p@shedcollective.org>
2025-08-22 09:32:12 +0100

    chore: Fix syntax error

--- src/Database/Migration/Migration3.php
+++ src/Database/Migration/Migration3.php
@@ -30,6 +30,6 @@ class Migration3 implements Interfaces\Database\Migration
         $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHARACTER SET = utf8mb4;');
         $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `old_url` `old_url` VARCHAR(500) CHARACTER SET utf8mb4 NULL DEFAULT NULL;');
         $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `new_url` `new_url` VARCHAR(500) CHARACTER SET utf8mb4 NULL DEFAULT NULL;');
-        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `type` `type` ENUM('301','302') CHARACTER SET utf8mb4 NULL DEFAULT '301';');
+        $this->query('ALTER TABLE `{{NAILS_DB_PREFIX}}redirect` CHANGE `type` `type` ENUM(\'301\',\'302\') CHARACTER SET utf8mb4 NULL DEFAULT \'301\';');
     }
 }
